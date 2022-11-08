ef10f4caa35668022c8cf6b9573a1e3f4ed5d37e
Emil Maria James <ponchinjames@gmail.com>
2022-11-08 21:43:02 +0530

    Practice qn 4 pattern 6

--- practice_4_pattern6.py
+++ practice_4_pattern6.py
@@ -1,11 +1,11 @@
 rows = int(input("Enter the number of rows: "))
 # Top half
-for row in range(0, rows+1):
-    for star in range(0, row + 1):
+for row in range(0, rows):
+    for star in range(0, row ):
         print("*", end=' ')
     print()
 # Bottom half
-for row in range(rows + 1, 0, -1):
-    for star in range(0, row - 1):
+for row in range(rows, 0, -1):
+    for star in range(0, row ):
         print("*", end=' ')
     print()
